19c4dda053e03bba0f5e78dd2f6105045f848fd8
Tony Gorez <gorez.tony@gmail.com>
2024-02-03 18:47:38 +0100

    feat: add helper

--- hashaddon.cc
+++ hashaddon.cc
@@ -9,6 +9,22 @@
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
+const char hexLookup[] = "0123456789abcdef";
+std::string hashToHex(const unsigned char *hash, size_t length) {
+  std::string hexStr;
+  hexStr.reserve(length * 2); // Pre-allocate space for the hexadecimal string
+
+  for (size_t i = 0; i < length; ++i) {
+    const unsigned char val = hash[i];
+    hexStr.push_back(hexLookup[val >> 4]);   // Extract the high nibble (4 bits)
+                                             // and find the hex character
+    hexStr.push_back(hexLookup[val & 0x0F]); // Extract the low nibble and find
+                                             // the hex character
+  }
+
+  return hexStr;
+}
+
 Napi::Array hash(const Napi::CallbackInfo &info) {}
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
